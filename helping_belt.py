dc050488d7f1edc4c0489a7db5c037b77f43bb76
ezeakis <ezeakis@gmail.com>
2019-03-27 06:20:53 +0200

    Create helping_belt.py

---
+++ helping_belt.py
@@ -0,0 +1,12 @@
+from gpiozero import DistanceSensor, Buzzer
+from time import sleep
+
+sensor = DistanceSensor(echo=18, trigger=17)
+b = Buzzer(3)
+
+while True:
+    d = sensor.distance * 100
+    print(d)
+    if d < 20:
+        b.beep(n=1)
+    sleep(1)
